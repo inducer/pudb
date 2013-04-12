e47ffd3e7e83d7c575fce312bb7d8ba0fa54e82d
Andreas Kloeckner <inform@tiker.net>
2013-04-12 09:13:49 -0400

    Fix warning message if the signal handler couldn't be set.

--- pudb/__init__.py
+++ pudb/__init__.py
@@ -151,7 +151,7 @@ def set_interrupt_handler(interrupt_signal=DEFAULT_SIGNAL):
         import sys
         from warnings import warn
         warn("setting interrupt handler on signal %d failed: %s"
-                % (interrupt_signal, format_exception(sys.exc_info())))
+                % (interrupt_signal, "".join(format_exception(sys.exc_info()))))
 
 def post_mortem(exc_info=None):
     if exc_info is None:
