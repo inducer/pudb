7033334e22f3fe31349675acc3c082fca39eb213
discort <odiscort@gmail.com>
2017-04-11 21:29:20 +0300

    added base tests for source code providers

---
+++ test/test_source_code_providers.py
@@ -0,0 +1,36 @@
+import pytest  # noqa: F401
+
+from pudb.debugger import NullSourceCodeProvider, FileSourceCodeProvider, DirectSourceCodeProvider
+from pudb.source_view import SourceLine
+
+
+class TestNullSourceCodeProvider:
+    def test_get_lines(self, mocker):
+        provider = NullSourceCodeProvider()
+        result = provider.get_lines(mocker.Mock())
+        assert len(result) == 10
+        assert isinstance(result[0], SourceLine)
+
+
+class TestFileSourceCodeProvider:
+    def test_string_file_name(self, mocker):
+        mock_debugger = mocker.Mock()
+        mock_debugger.canonic = mocker.Mock(return_value='<string>')
+        provider = FileSourceCodeProvider(mock_debugger, 'test file name')
+        result = provider.get_lines(mocker.MagicMock())
+        assert len(result) == 1
+        assert isinstance(result[0], SourceLine)
+
+    def test_get_lines(self, mocker):
+        provider = FileSourceCodeProvider(mocker.Mock(), 'test file name')
+        result = provider.get_lines(mocker.MagicMock())
+        assert len(result) == 1
+        assert isinstance(result[0], SourceLine)
+
+
+class TestDirectSourceCodeProvider:
+    def test_get_lines(self, mocker):
+        provider = DirectSourceCodeProvider(mocker.Mock(), 'test code')
+        result = provider.get_lines(mocker.Mock())
+        assert len(result) == 1
+        assert isinstance(result[0], SourceLine)
