c67f80c65039c6da39f7708669edfd3310eb026e
xywei <wxy0516@gmail.com>
2020-12-17 20:07:11 -0600

    Fix missed renaming

--- pudb/settings.py
+++ pudb/settings.py
@@ -272,7 +272,7 @@ def edit_config(ui, conf_dict):
 
     shell_info = urwid.Text("This is the shell that will be "
             "used when you hit '!'.\n")
-    shells = ["internal", "classic", "ipython", "ipython-kernel", "bpython",
+    shells = ["internal", "classic", "ipython", "ipython_kernel", "bpython",
               "ptpython", "ptipython"]
     known_shell = conf_dict["shell"] in shells
     shell_edit = urwid.Edit(edit_text=conf_dict["custom_shell"])
