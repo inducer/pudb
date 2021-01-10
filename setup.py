b177c84da93dfcd55ed3c924107ce19001e4de16
Andreas Kloeckner <inform@tiker.net>
2021-01-10 17:09:09 -0600

    Black setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from setuptools import setup
 from pudb import VERSION
 
 import sys
+
 py_version_major = sys.version_info[0]
 if py_version_major == 3:
     PY_VERSION = str(py_version_major)
 else:
-    PY_VERSION = ''
+    PY_VERSION = ""
 
 try:
     readme = open("README.rst")
@@ -16,42 +17,44 @@ try:
 finally:
     readme.close()
 
-setup(name='pudb',
-      version=VERSION,
-      description='A full-screen, console-based Python debugger',
-      long_description=long_description,
-      author='Andreas Kloeckner',
-      author_email='inform@tiker.net',
-      python_requires="~=3.6",
-      install_requires=[
-          "urwid>=1.1.1",
-          "pygments>=1.0",
-          ],
-      test_requires=[
-          "pytest>=2",
-          ],
-      url='https://github.com/inducer/pudb',
-      classifiers=[
-          "Development Status :: 4 - Beta",
-          "Environment :: Console",
-          "Environment :: Console :: Curses",
-          "Intended Audience :: Developers",
-          "License :: OSI Approved :: MIT License",
-          "Natural Language :: English",
-          "Operating System :: POSIX",
-          "Operating System :: Unix",
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 3",
-          "Topic :: Software Development",
-          "Topic :: Software Development :: Debuggers",
-          "Topic :: Software Development :: Quality Assurance",
-          "Topic :: System :: Recovery Tools",
-          "Topic :: System :: Software Distribution",
-          "Topic :: Terminals",
-          "Topic :: Utilities",
-          ],
-      packages=["pudb"],
-      entry_points={
-          'console_scripts': ['pudb' + PY_VERSION + ' = pudb.run:main'],
-          'gui_script': []},
-      )
+setup(
+    name="pudb",
+    version=VERSION,
+    description="A full-screen, console-based Python debugger",
+    long_description=long_description,
+    author="Andreas Kloeckner",
+    author_email="inform@tiker.net",
+    python_requires="~=3.6",
+    install_requires=[
+        "urwid>=1.1.1",
+        "pygments>=1.0",
+    ],
+    test_requires=[
+        "pytest>=2",
+    ],
+    url="https://github.com/inducer/pudb",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: Console :: Curses",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Debuggers",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: System :: Recovery Tools",
+        "Topic :: System :: Software Distribution",
+        "Topic :: Terminals",
+        "Topic :: Utilities",
+    ],
+    packages=["pudb"],
+    entry_points={
+        "console_scripts": ["pudb" + PY_VERSION + " = pudb.run:main"],
+        "gui_script": [],
+    },
+)
