0432e986769e1e7c2f0f5572004fd0de6507f1a8
Michael van der Kamp <michael.vanderkamp@usask.ca>
2021-08-22 22:57:01 -0600

    Don't modify global state when linking styles
    Instead of changing the global INHERITANCE_MAP, provide a fresh local
    dict of inheritance overrides from inside get_palette.

--- pudb/theme.py
+++ pudb/theme.py
@@ -91,7 +91,7 @@ BASE_STYLES = {
 # Map styles to their parent. If a style is not defined, use the parent style
 # recursively.
 # focused > highlighted > current > breakpoint line/disabled breakpoint
-CLEAN_INHERITANCE_MAP = {
+INHERITANCE_MAP = {
     # {{{ general ui
     "label": "background",
     "header": "background",
@@ -231,10 +231,10 @@ CLEAN_INHERITANCE_MAP = {
     "backtick":     "string",
     # }}}
 }
-INHERITANCE_MAP = CLEAN_INHERITANCE_MAP.copy()
 
 
-def get_style(palette_dict: dict, style_name: str):
+def get_style(palette_dict: dict, style_name: str,
+              inheritance_overrides: dict) -> dict:
     """
     Recursively search up the style hierarchy for the first style which has
     been defined, and add it to the palette_dict under the given style_name.
@@ -247,14 +247,16 @@ def get_style(palette_dict: dict, style_name: str):
             palette_dict[style_name] = style
         return style
     except KeyError:
-        parent_name = INHERITANCE_MAP[style_name]
-        style = replace(get_style(palette_dict, parent_name), name=style_name)
+        parent_name = inheritance_overrides.get(
+            style_name,
+            INHERITANCE_MAP[style_name],
+        )
+        style = replace(
+            get_style(palette_dict, parent_name, inheritance_overrides),
+            name=style_name
+        )
         palette_dict[style_name] = style
         return style
-
-
-def link(child: str, parent: str):
-    INHERITANCE_MAP[child] = parent
 
 # }}}
 
@@ -264,8 +266,7 @@ def get_palette(may_use_fancy_formats: bool, theme: str = "classic") -> list:
     Load the requested theme and return a list containing all palette entries
     needed to highlight the debugger UI, including syntax highlighting.
     """
-    # undo previous link() calls
-    INHERITANCE_MAP.update(CLEAN_INHERITANCE_MAP)
+    inheritance_overrides = {}
 
     if may_use_fancy_formats:
         def add_setting(color, setting):
@@ -273,6 +274,9 @@ def get_palette(may_use_fancy_formats: bool, theme: str = "classic") -> list:
     else:
         def add_setting(color, setting):
             return color
+
+    def link(child: str, parent: str):
+        inheritance_overrides[child] = parent
 
     # {{{ themes
 
@@ -866,7 +870,7 @@ def get_palette(may_use_fancy_formats: bool, theme: str = "classic") -> list:
 
     # Apply style inheritance
     for style_name in set(INHERITANCE_MAP.keys()).union(BASE_STYLES.keys()):
-        get_style(palette_dict, style_name)
+        get_style(palette_dict, style_name, inheritance_overrides)
 
     palette_list = [
         astuple(entry)
