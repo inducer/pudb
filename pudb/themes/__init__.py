5293730bca36de7b4ca29cb22d6d15af78fae669
Michael van der Kamp <michael.vanderkamp@usask.ca>
2023-09-17 21:21:47 -0600

    Add light gray 256 theme

--- pudb/themes/__init__.py
+++ pudb/themes/__init__.py
@@ -2,6 +2,7 @@ from .classic import palette_dict as Classic
 from .agr_256 import palette_dict as Agr256
 from .solarized import palette_dict as Solarized
 from .dark_vim import palette_dict as DarkVim
+from .gray_light_256 import palette_dict as GrayLight256
 from .midnight import palette_dict as Midnight
 from .mono import palette_dict as Mono
 from .monokai import palette_dict as Monokai
@@ -17,5 +18,6 @@ THEMES = {
     "solarized": Solarized,
     "mono": Mono,
     "agr-256": Agr256,
+    "gray-light-256": GrayLight256,
     "monokai-256": Monokai256,
 }
---
+++ pudb/themes/gray_light_256.py
@@ -0,0 +1,33 @@
+from pudb.themes.utils import add_setting, link
+
+palette_dict = {
+    # {{{ base styles
+    "background": ("h232", "h248"),
+    "selectable": ("h232", "h252"),
+    "focused selectable": ("h232", "h251"),
+    "highlighted": (add_setting("h234", "bold, underline"), "h252"),
+    "hotkey": (add_setting("h232", "underline"), "h248"),
+    # }}}
+    # {{{ general ui
+    "warning": (add_setting("h232", "bold"), "h254"),
+    "group head": (add_setting("h232", "bold"), "h248"),
+    "dialog title": (add_setting("h232", "underline, bold"), "h248"),
+    # }}}
+    # {{{ source view
+    "source": ("h235", "h253"),
+    "current source": (add_setting("h232", "underline"), "h253"),
+    "line number": ("h244", "h253"),
+    # }}}
+    # {{{ sidebar
+    "sidebar two": (add_setting("h234", "bold"), "h252"),
+    "focused sidebar two": (add_setting("h234", "bold"), "h251"),
+    "sidebar three": ("h239", "h252"),
+    "focused sidebar three": ("h239", "h251"),
+    # }}}
+    # {{{ Code syntax
+    "exception":   (add_setting("h236", "underline"), "h253"),
+    "class":       (add_setting("h234", "bold, underline"), "h253"),
+    "keyword":     (add_setting("h234", "bold"), "h253"),
+    "comment":     ("h244", "h253"),
+    # }}}
+}
