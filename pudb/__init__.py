f5706ea6d04dec03914d59a1ccc02cf95a36f16d
Sergiy Redko <sergiy.redko@altavec.com>
2024-03-27 08:17:37 +1100

    revert global restart hotkey

--- pudb/__init__.py
+++ pudb/__init__.py
@@ -172,10 +172,6 @@ def runscript(mainpyfile, args=None, pre_run="", steal_output=False,
             import urwid
             pre_run_edit = urwid.Edit("", pre_run)
 
-            if dbg.restart_requested():
-                dbg.clear_restart()
-                break
-
             if not load_config()["prompt_on_quit"]:
                 return
 
