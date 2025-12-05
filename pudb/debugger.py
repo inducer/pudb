0056c27e51add962af03ef481dbba4403b6da6b7
Andreas Kloeckner <inform@tiker.net>
2025-12-05 10:53:08 -0600

    Bump version to 2025.1.4

--- pudb/debugger.py
+++ pudb/debugger.py
@@ -2697,7 +2697,7 @@ Error with jump. Note that jumping only works on the topmost stack frame.
                 self.message("Package 'pygments' not found. "
                         "Syntax highlighting disabled.")
 
-        WELCOME_LEVEL = "e054"  # noqa
+        WELCOME_LEVEL = "e055"  # noqa
         if CONFIG["seen_welcome"] < WELCOME_LEVEL:
             CONFIG["seen_welcome"] = WELCOME_LEVEL
             from pudb import VERSION
@@ -2713,6 +2713,10 @@ Error with jump. Note that jumping only works on the topmost stack frame.
                     "If you're new here, welcome! The help screen "
                     "(invoked by hitting '?' after this message) should get you "
                     "on your way.\n"
+
+                    "\nChanges in version 2025.1.4:\n\n"
+                    "- Work around compatibility break in urwid 3.0.4 "
+                    "(reported by Marc Salat)\n"
 
                     "\nChanges in version 2025.1.3:\n\n"
                     "- Actually fix module picking\n"
